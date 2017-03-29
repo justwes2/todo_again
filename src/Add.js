48a7c171079d4fd40d59d641a615fef669e56e26
Wes <wes.coffay@gmail.com>
2017-03-29 14:10:35 -0400

    todos print to page

--- src/Add.js
+++ src/Add.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react';
 
-const todo = []
+// const todo = []
 
 class Add extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      list: ["laundry", "shopping"],
       post: '',
       complete: false
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event){
+    const target = event.target
+    const value = target.value
+    this.setState({
+      post: value
+    })
   }
   handleSubmitEvent(event){
     event.preventDefault()
-    console.log(event.target);
-    // addstate to 
-  }
-  handleChange(event){
-    console.log(event.target.value);
-    // setstate
+    this.setState({
+      list:  this.state.list.concat([this.state.post]),
+      post: ''
+    }, () => {
+      console.log(this.state.list);
+      let form = document.getElementById('textbox')
+      form.reset()
+    })
   }
 
   render() {
+    // console.log(this.state.list);
+    let print = this.state.list.map((item, index) => {
+      return(
+        <div key={index}>
+        <p>{item}</p>
+        </div>
+      )
+    })
     return(
       <div>
       <h3>Add a todo</h3>
-      <form onSubmit= {(event) => this.handleSubmitEvent(event)}>
-      <input type="text" placeholder="Add your todo" onChange={this.handleChange}/>
+      <form id="textbox" onSubmit= {(event) => this.handleSubmitEvent(event)}>
+      <input type="text"  placeholder="Add your todo" onChange={this.handleChange}/>
       <button type="submit">+</button>
       </form>
+
+        {print}
+
       </div>
     )
   }
